190db2c5a70c19343f66e60cea28d1b918a2f1ac
David Vidic <davidgvidic@gmail.com>
2024-06-20 00:46:14 -0500

    Initial Commit

---
+++ portfolio/parser/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+# Goals with instagram parser app:
+#   First landing page is just file upload
+#   then it takes you to second page which outputs the data it parsed out when it downloaded it
+#   show relationship with data such as
+#   who follows you but you dont follow them
+#   who you follow but don't follow you back
+#   you can then choose to do more relationship with the data if you choose
+
